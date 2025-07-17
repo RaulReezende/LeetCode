dbb65ef01774f383621312189b933cce7229f9ae
Raul Rezende <raul31fish@gmail.com>
2025-07-17 15:17:08 -0300

    testing

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,6 +4,6 @@ using Algorithms.DP;
 KnapSack01 cs = new();
 
 const int capacity = 10;
-int[] V = [1, 4, 8, 5];
+int[] V = [1, 4, 8, 6];
 int[] W = [3, 3, 5, 6];
 Console.WriteLine(KnapSack01.KnapSackByMe(capacity, W, V));
